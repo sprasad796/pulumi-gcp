be548cd63b6c71659677482eb69314d71457c9ac
Swati Prasad <sprasad796.uplevel@gmail.com>
2025-09-02 14:16:04 -0400

    Addiing coding test

--- __main__.py
+++ __main__.py
@@ -1,10 +1,19 @@
 """A Google Cloud Python Pulumi program"""
 
 import pulumi
+import pulumi_gcp as gcp
 from pulumi_gcp import storage
 
 # Create a GCP resource (Storage Bucket)
-bucket = storage.Bucket('my-bucket', location="US")
+#bucket = storage.Bucket('my-bucket', location="US")
 
 # Export the DNS name of the bucket
-pulumi.export('bucket_name', bucket.url)
+#pulumi.export('bucket_name', bucket.url)
+pulumi.export("a", "banana")
+pulumi.export("b", pulumi.Output.secret("miaow"))
+pulumi.export("c", pulumi.Output.secret("woof"))
+pulumi.export("d", pulumi.Output.secret({"a": 1, "b": 2}))
+
+#Config
+config = pulumi.Config();
+print('config: {}',config);
